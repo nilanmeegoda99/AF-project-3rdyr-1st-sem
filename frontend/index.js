c43a2fdee7ae7d4e18d8f3b5ad2daff9f965d726
nilanmeegoda99 <nilanmeegoda@gmail.com>
2021-06-02 00:03:20 +0530

    test commit

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 render(
-    <h1>Hello ICAF</h1>, document.getElementById('root')
+    <h1>Hello fgdfgdfgdfgdfgdfgdfg</h1>, document.getElementById('root')
 );
 
 
