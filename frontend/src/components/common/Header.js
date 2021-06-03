ad5ebdd4716795417934bc58412d8d79ecdf0b6e
nilanmeegoda99 <nilanmeegoda@gmail.com>
2021-06-04 00:00:01 +0530

    Change the title name from ICAC to ICAF

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
         return (
             <header>
                 <Navbar bg="dark" variant="dark" expand="lg" fixed="top" >
-                    <Navbar.Brand href="#home">ICAC</Navbar.Brand>
+                    <Navbar.Brand href="#home">ICAF</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
