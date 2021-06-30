0b6c9ddfbde0fcec8199551ee77aa96c116d4f0c
DreX7001 <ayodhyaratnayake7001@gmail.com>
2021-06-30 22:31:13 +0530

    final commit 2

--- frontend/src/components/__test__/comp.test.js
+++ frontend/src/components/__test__/comp.test.js
@@ -7,6 +7,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Login from "../public/Login";
 import Register from "../public/Register";
+import AboutUs from "../public/AboutUs";
+import Home from "../public/Home";
+import Footer from "../common/Footer";
+import AttendeeInstructions from "../users/guest/AttendeeInstructions";
+import ResearchInstructions from "../users/guest/ResearchInstructions";
+import WorkshopInstructions from "../users/guest/WorkshopInstructions";
 
 test("render without crashing", () => {
   const root = document.createElement("div");
@@ -18,7 +24,32 @@ test("render without crashing", () => {
   ReactDOM.render(<Register />, root);
 });
 
-test("use jsdom in this test file", () => {
-  const element = document.createElement("div");
-  expect(element).not.toBeNull();
+test("render without crashing", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<AboutUs />, root);
+});
+
+test("render without crashing", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<Home />, root);
+});
+
+test("render without crashing", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<Footer />, root);
+});
+
+test("render without crashing", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<AttendeeInstructions />, root);
+});
+
+test("render without crashing", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<ResearchInstructions />, root);
+});
+
+test("render without crashing", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<WorkshopInstructions />, root);
 });
